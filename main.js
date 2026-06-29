0ac6a63ec8e6e0ca083416f21cc1c73559bd530d
yymmt <y.ymmt1984@gmail.com>
2026-06-29 09:53:42 +0900

    Fix vertical Y-axis texture flipping issue in WebGL pattern rendering

--- main.js
+++ main.js
@@ -204,6 +204,7 @@ function loadDrawingTexture(id) {
                         if (gl) {
                             const texture = gl.createTexture();
                             gl.bindTexture(gl.TEXTURE_2D, texture);
+                            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
                             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
                             
                             const isPowerOf2 = (val) => (val & (val - 1)) === 0;
